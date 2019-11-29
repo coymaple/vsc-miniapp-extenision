106987ed5e2ddb53f214c7b168c736508bd2a207
coymaple <coymaple@gmail.com>
2019-11-29 14:56:43 +0800

    2019.11.29

--- src/WxmlFormatter.ts
+++ src/WxmlFormatter.ts
@@ -1,12 +1,51 @@
 
-import {TextDocument,TextEdit} from 'vscode';
+// import {TextDocument,TextEdit} from 'vscode';
 
 
-export default class WxmlFormatter{
-  provideDocumentFormattingEdits(doc: TextDocument): TextEdit[] {
-    const firstLine = doc.lineAt(0);
+// export default class WxmlFormatter{
+//   provideDocumentFormattingEdits(doc: TextDocument): TextEdit[] {
+//     const firstLine = doc.lineAt(0);
+
+//     return [TextEdit.insert(firstLine.range.start, '42\n')];
+//   }
+// }
+
+import * as vscode from 'vscode';
+const prettier = require('prettier');
+const fs = require('fs');
+
+import {
+  TextDocument,
+  FormattingOptions,
+  CancellationToken,
+  ProviderResult,
+  TextEdit,
+  WorkspaceEdit
+} from 'vscode';
+
+const format = () => {
+  const { activeTextEditor } = vscode.window;
+  if (activeTextEditor) {
+    const { document } = activeTextEditor;
+    if(document.languageId === 'wxml'){
+      const edit = new vscode.WorkspaceEdit();
+      return vscode.workspace.applyEdit(edit);
+    }
+    // prettier.format(source,'')
+  
     
-    return [TextEdit.insert(firstLine.range.start, '42\n')];
+  
   }
+};
+
+class WxmlFormatter implements vscode.DocumentFormattingEditProvider{
+  provideDocumentFormattingEdits(document: TextDocument, options: FormattingOptions, token: CancellationToken): ProviderResult<TextEdit[]>{
+    
+  }
+	
 }
 
+export default format;
+export {
+  WxmlFormatter
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,12 @@
 import * as vscode from 'vscode';
 import { getRuleSetOperator } from './RuleSet';
-import WxmlFormatter from './WxmlFormatter';
+import format,{ WxmlFormatter} from './WxmlFormatter';
+
 const wxmlFormatter = new WxmlFormatter();
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let disp = vscode.commands.registerCommand('extension.formatwxml', () => {
-		const { activeTextEditor } = vscode.window;
-
-		if (activeTextEditor) {
-			const { document } = activeTextEditor;
-			console.log(document);
-			const edit = new vscode.WorkspaceEdit();
-			// edit.insert(document.uri, firstLine.range.start, '42\n');
-
-			return vscode.workspace.applyEdit(edit);
-
-		}
-	});
+	let disp = vscode.commands.registerCommand('extension.formatwxml', format);
 	context.subscriptions.push(disp);
 
 	let ruleSetOperator = getRuleSetOperator();
@@ -26,6 +15,5 @@ export function activate(context: vscode.ExtensionContext) {
 		context.subscriptions.push(disposable);
 	}
 
-	vscode.languages.registerDocumentFormattingEditProvider({ scheme: 'file', language: 'wxml' }, wxmlFormatter);
+	// vscode.languages.registerDocumentFormattingEditProvider({ scheme: 'file', language: 'wxml' }, wxmlFormatter);
 }
-
